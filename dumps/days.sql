82b9dd9ad216c99dcbcdd3ada4bc8a7bd6432415
Teddy Ditchek <t.ditchek@gmail.com>
2016-08-18 14:25:41 -0400

    updated schedule db to inlude thursday

--- dumps/days.sql
+++ dumps/days.sql
@@ -1,5 +1,8 @@
 INSERT INTO `sDay` (`dayID`, `displayName`, `available`, `text_red`, `text_green`, `text_blue`) VALUES
-(1, 'Thursday', 1, 255, 255, 255),
-(2, 'Friday', 1, 255, 255, 255),
-(3, 'Shabbos', 1, 255, 255, 255),
-(4, 'Sunday', 1, 255, 255, 255);
+(1, 'Thursday', 0, 255, 255, 255),
+(2, 'Friday', 0, 255, 255, 255),
+(3, 'Shabbos', 0, 255, 255, 255),
+(4, 'Sunday', 0, 255, 255, 255),
+(5, 'Monday', 0, 255, 255, 255),
+(6, 'Tuesday', 0, 255, 255, 255),
+(7, 'Wednesday', 0, 255, 255, 255);
